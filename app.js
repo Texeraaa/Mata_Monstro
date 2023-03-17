89e17306a7476d6756ee54f3cc7863b5d39f2891
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2023-03-17 14:23:40 -0300

    finalizado listas e começando logs

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ new Vue({
         data: {
             vidaMonstro: 100,
             vidaPlayer: 100,
-            logDanoMonstro: [],
-            logDanoPlayer: [],
-            logs: [],
+            jogoRodando: false,
+            ganhou: true,
+            lifeDangerPlayer: false,
+            lifeDangerMonstro: false,
+
+            listaLogs: [
+                [],   //lista dano do Monstro
+                [],   //lista dano do Player
+                [],   //lista da cura do Player
+            ],
+        },
+        created(){
+          setInterval(() => {
+            this.validaEstadoJogo()
+          }, 1);  
         },
         methods:{
             danoMonstro(){
@@ -13,39 +25,70 @@ new Vue({
                 this.danoDoMonstro = danoMonstro
                 console.log("O Monstro deu ", danoMonstro, " De dano")
                 this.vidaPlayer -= danoMonstro;
-                this.logs.push(danoMonstro)
+                this.listaLogs[0].push(danoMonstro)
                 return
             },
-            curar(){
-
-            },
-            ataque_especial(){
-                const danoPlayer = Math.floor(Math.random() * (21 - 15) + 15)
-                this.danoPlayer = danoPlayer  
-                this.vidaMonstro -= danoPlayer;
-                // console.log("O Player deu ", danoPlayer, " De dano")
-                this.danoMonstro()
-                this.logs.push(danoPlayer)
-
-            },
             atacar(){
-                this.danoMonstro()
-                const danoPlayer = Math.floor(Math.random() * 9)
+                this.danoMonstro() //inicializa o dano do monstro
+                const danoPlayer = Math.floor(Math.random() * 9) //randomiza o dano do player de 0 a 8
+                this.danoPlayer = danoPlayer  //cria objeto
+                this.vidaMonstro -= danoPlayer; //tira vida do monstro
+                this.listaLogs[1].push(danoPlayer) //manda para a array logs
 
-                this.danoPlayer = danoPlayer
-                
+                // teste de console
                 // console.log("O Player deu ", danoPlayer, " De dano")
                 // console.log("O Monstro deu ", danoMonstro, " De dano")
+            
+            },  
+            ataque_especial(){
                 
+                this.danoMonstro() //inicializa o dano do monstro
+                const danoPlayer = Math.floor(Math.random() * (17 - 10) + 10) //randomiza o dano do player de 10 a 15
+                this.danoPlayer = danoPlayer  
                 this.vidaMonstro -= danoPlayer;
+                console.log("O Player deu ", danoPlayer, " De dano")
+                this.listaLogs[1].push(danoPlayer)
+            },    
+            curar(){
                 
-                this.logs.push(danoPlayer)
+                this.danoMonstro()
+                const curaPlayer = Math.floor(Math.random() * 10);
+                this.curaPlayer = curaPlayer;
+                this.vidaPlayer += curaPlayer;
+                console.log("O player curou ", curaPlayer, " de vida")
+                this.listaLogs[2].push(curaPlayer)
+            },
+            validaEstadoJogo(){
+                if(this.vidaMonstro <= 0){
+                    this.jogoRodando = false
+                    this.ganhou = true
+                }else if(this.vidaPlayer <= 0){
+                    this.jogoRodando = false
+                    this.ganhou = false
+                }
+                
+                if(this.vidaPlayer <= 20){
+                    this.lifeDangerPlayer = true
+                }else if(this.vidaMonstro <= 20){
+                    this.lifeDangerMonstro = true
+                }
+            },
+            resetaComponentes(){
+                this.vidaMonstro = 100
+                this.vidaPlayer = 100
+                this.jogoRodando = true
+                this.lifeDangerMonstro = false
+                this.lifeDangerPlayer = false
+            }
+        },
+        computed:{
+            aposIniciar(){
 
-            },       
+            }
         },
         watch:{
-             logs(valorAtual, valorFinal){
-                // console.log(valorAtual)
+            listaLogs(valorAtual, valorFinal){
+                console.log(valorAtual)
             }
-        }
+        },
 })
